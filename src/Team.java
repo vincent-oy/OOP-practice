d4d0c0f74192b0ff470c4989ff52614c578da184
vincent-oy <vincentoy1108@gmail.com>
2025-09-11 19:38:15 +0800

    Initial commit

---
+++ src/Team.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Team {
+    //Attributes
+    private String name;
+    private ArrayList<Match> matches;
+    private int points;
+    private int goalsFor;
+    private int goalsAgainst;
+
+    //Constructor
+    public Team(String name) {
+        this.name = name;
+        this.matches = new ArrayList<>();   //start with an empty match list
+        this.points = points;
+        this.goalsFor = goalsFor;
+        this.goalsAgainst = 
+    }
+
+}
