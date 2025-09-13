14c667940d0e302fe8762ece4c8a73a24de8f5ca
vincent-oy <vincentoy1108@gmail.com>
2025-09-13 17:35:14 +0800

    Use ArrayList collections and add match comments

--- src/League.java
+++ src/League.java
@@ -1,36 +1,36 @@
 import java.util.ArrayList;
 
 public class League {
-    //Attributes
     private String name;
+    // Using ArrayList directly as requested
     private ArrayList<Team> teams;
     private ArrayList<Match> matches;
 
-    
     public League(String name) {
         this.name = name;
         this.teams = new ArrayList<>();
         this.matches = new ArrayList<>();
     }
 
-    public addTeam(Team t) {
+    public void addTeam(Team t) {
         teams.add(t);
-   }
+    }
 
-    public scheduleMatch(Match m) {
+    public void scheduleMatch(Match m) {
         matches.add(m);
     }
 
-    public updateStandings() {
-        for (Match eachMatch : matches) {
-            eachMatch.homeTeam.updateStats(eachMatch);
-            eachMatch.awayTeam.updateStats(eachMatch);
+    public void updateStandings() {
+        for (Match match : matches) {
+            match.getHomeTeam().updateStats(match);
+            match.getAwayTeam().updateStats(match);
         }
     }
 
-    public printStandings() {
-        for (Team eachTeam : teams) {
-            System.out.println(eachTeam.getRecord());
+    public void printStandings() {
+        for (Team team : teams) {
+            System.out.println(team.getRecord());
         }
     }
 }
+
--- src/Match.java
+++ src/Match.java
@@ -5,43 +5,40 @@ public class Match {
     private int homeScore;
     private int awayScore;
 
-    public Match(Team homeTeam, Team awayTeam) {
+    public Match(String date, Team homeTeam, Team awayTeam) {
         this.date = date;
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
     }
 
-    public setResult(int homeScore, int awayScore) {
+    public void setResult(int homeScore, int awayScore) {
         this.homeScore = homeScore;
         this.awayScore = awayScore;
     }
 
-    public toString() {
-        // return homeTeam.name + " " + homeScore + " - " + awayScore + " " + awayTeam.name + "(" + date + ")";
-        // "Tigers 2 - 1 Eagles (12/09/2025)"
-        return String.format("%s %d - %d %s (%s)", homeTeam.name, homeScore, awayScore, awayTeam.name, date);
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    @Override
+    public String toString() {
+        // return homeTeam.getName() + " " + homeScore + " - " + awayScore +
+        //        " " + awayTeam.getName() + " (" + date + ")";
+        // Example: "Tigers 2 - 1 Eagles (12/09/2025)"
+        return String.format("%s %d - %d %s (%s)",
+                homeTeam.getName(), homeScore, awayScore, awayTeam.getName(), date);
     }
 }
-public class Match {
-    private String date;
-    private Team homeTeam;
-    private Team awayTeam;
-    private int homeScore;
-    private int awayScore;
 
-    public Match(Team homeTeam, Team awayTeam) {
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
-    }
-
-    public setResult(int homeScore, int awayScore) {
-        this.homeScore = homeScore;
-        this.awayScore = awayScore;
-    }
-
-    public toString() {
-        // return homeTeam.name + " " + homeScore + " - " + awayScore + " " + awayTeam.name + "(" + date + ")";
-        // "Tigers 2 - 1 Eagles (12/09/2025)"
-        return String.format("%s %d - %d %s (%s)", homeTeam.name, homeScore, awayScore, awayTeam.name, date);
-    }
-}
