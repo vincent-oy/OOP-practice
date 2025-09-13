704bfdc535b97fcb1f158b2791aead09a7c45c85
vincent-oy <vincentoy1108@gmail.com>
2025-09-13 16:43:35 +0800

    Third commit

--- src/League.java
+++ src/League.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+
 public class League {
+    //Attributes
+    private String name;
+    private ArrayList<Team> teams;
+    private ArrayList<Match> matches;
+
     
+    public League(String name) {
+        this.name = name;
+        this.teams = new ArrayList<>();
+        this.matches = new ArrayList<>();
+    }
+
+    public addTeam(Team t) {
+        teams.add(t);
+   }
+
+    public scheduleMatch(Match m) {
+        matches.add(m);
+    }
+
+    public updateStandings() {
+        for (Match eachMatch : matches) {
+            eachMatch.homeTeam.updateStats(eachMatch);
+            eachMatch.awayTeam.updateStats(eachMatch);
+        }
+    }
+
+    public printStandings() {
+        for (Team eachTeam : teams) {
+            System.out.println(eachTeam.getRecord());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,8 @@ public class Main {
         System.out.println("Hello, World!");
     }
 }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
